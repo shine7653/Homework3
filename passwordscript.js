8dd6ca0114aa7ecef2aaeb3c7a625a9c40bb24ab
Sinae Kim <54830877+shine7653@users.noreply.github.com>
2019-10-05 16:12:09 -0700

    Generate done

--- passwordscript.js
+++ passwordscript.js
@@ -1,96 +1,69 @@
+var passBox = document.querySelector("#password");
+var generate = document.querySelector("#generate");
+// 3. Function : Computer execute to choose random characters among what the user chose
+//               (When user click the generate button, password will keep generating.)
+// 4. Print it out to Password page.
+// 
 
+var randomPassword = "";
+var passWord = "";
 
-
-
-var passWordGen = [];
-var randomPass = "";
-
-var spe = ["~","!","@","#","$","%","^","&","*","(",")","_","+"];
+var spe = ["~", "!", "@", "#", "$", "%", "^", "&", "*", "(", ")", "_", "+"];
 var num = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9];
-var low = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "z"];
+var lower = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "z"];
 var upper = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "Z"];
 
-
-function userLength() {
+// 1. Input : User choose the length (Ask repeatedly until choose right length)
+function chooseUserLength() {
     var x;
-    var numOK = false;
-    while (numOK !== true) {
+    var lengOK = false;
+    while (lengOK !== true) {
         x = Number.parseInt(prompt("How many characters do you want to make?"));
         if (x >= 8 && x <= 128) {
-            numOK = true;
-            alert("OK. Let's make your password");
+            lengOK = true;
+            alert("Your password length is " + x);
         } else {
             alert("You need to choose between 8 to 128");
         };
         console.log("User selected length: " + x);
     };
     return x;
-}
+};
 
-function userSpecial() {
-    var s = confirm("Do you want to use special Cahracter?")
-        if (s == true) {
-            alert("You are going to use special character.");
-        } else {
-            alert("You are not goint to use speaicl character.");
-        };
-        console.log("User use: " + s);
-    return s;
-}
+generate.addEventListener("click", function() {
+    randomPassword = "";
+    var passwordLength = chooseUserLength();
+    var s = confirm("Special");
+    var n = confirm("Number");
+    var u = confirm("Upper");
+    var l = confirm("Lower");
 
-function userNumber() {
-    var n = confirm("Do you want to use number?")
-        if (n == true) {
-            alert("You are going to use number.");
-        } else {
-            alert("You are not goint to use number.");
-        };
-        console.log("Usde use: " + n);
-    return n;
-}
-
-function userUpper() {
-    var u = confirm("Do you want to use upper Cahracter?")
-        if (u == true) {
-            alert("You are going to use upper character.");
-        } else {
-            alert("You are not goint to use upper character.");
-        };
-        console.log();
-    return u;
-}
-
-function userLower() {
-    var l = confirm("Do you want to use lower Cahracter?")
-        if (l == true) {
-            alert("You are going to use lower character.");
-        } else {
-            alert("You are not goint to use lowel character.");
-        };
-        console.log();
-    return l;
-}
-
-//make x random numbers
-
-//mix up the arrays of user chose
-
-//------------------------------------------------
-// ask user how many length
-var userLength = userLength();
-var userSpecial = userSpecial();
-var userNumber = userNumber();
-var userUpper = userUpper();
-var userLower = userLower();
-
-while (randomPass.length < userLength) {
-
-    if(userSpecial === true) {
-        random  * 
+    while (randomPassword.length < passwordLength) {
+        if( s === true ) {
+            randomPassword += spe[Math.floor(Math.random()*spe.length)];
+        } 
+        if ( n === true ) {
+            randomPassword += num[Math.floor(Math.random()*num.length)];
+        } 
+        if ( u === true ) {
+            randomPassword += upper[Math.floor(Math.random()*upper.length)];
+        } 
+        if ( l === true) {
+            randomPassword += lower[Math.floor(Math.random()*lower.length)];
+        }
     }
+    passBox.innerHTML = randomPassword;    
+
+    
+    // for (var i=0 ; i < length ; i++) {
+    //     passWord += randomPassword.charAt(Math.floor(Math.random() * Math.floor(randomPassword.length -1)))
+    // }
+
+})
+
+
+console.log(passWord);
 
 
 
-
-
-}
+/* passBox.innerHTML = randomPassword; */
